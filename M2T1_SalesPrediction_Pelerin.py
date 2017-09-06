07afae9e3e754a1a9f50f1c8a3602f003451c0d1
abcameron2004 <31710676+abcameron2004@users.noreply.github.com>
2017-09-06 17:29:16 -0400

    Add files via upload
    first program

---
+++ M2T1_SalesPrediction_Pelerin.py
@@ -0,0 +1,12 @@
+#CTI-110
+#M2T1 - Sales Prediction
+#Ilona Pelerin
+#August 28, 2017
+#
+totalSales = float(input("Enter the projected sales: "))
+
+annualProfit = totalSales * 0.23
+
+print("Annual profit is $", format (annualProfit, ",.2f"))
+
+
